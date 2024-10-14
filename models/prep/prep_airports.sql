d06a7f322adc68ac7a63e13fb9b2ead43ce1074b
Heiko <h.fraembs24@gmail.com>
2024-10-14 17:33:07 +0200

    Update prep_airports.sql

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,7 +1,7 @@
 WITH airports_reorder AS (
     SELECT faa
-            ,regions,
-            countries
+            ,regions
+            ,countries
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
